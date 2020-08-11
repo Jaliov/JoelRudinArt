57df4f0b956591d37450a66419bb386d0e67e7fa
Joel Rudin <jarud55@gmail.com>
2020-08-11 17:48:36 -0400

    image/audio tweaks

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function audioLoad() {
-  var x = document.getElementById('myAudio').autoplay;
-  document.getElementById('myAudio').innerHTML = x;
-}
-document.body.onload = function () {
+window.onload = function () {
+  document.getElementById('audio').play();
   Carousel();
 };
