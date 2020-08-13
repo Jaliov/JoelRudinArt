46a9200e74e6d80e57fc3300f444148aa6535bb0
Joel Rudin <jarud55@gmail.com>
2020-08-13 11:25:36 -0400

    navbar script

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ window.onload = function () {
   document.getElementById('audio').play();
   Carousel();
 };
+
+document.getElementById('navLoad').innerHTML =
+  "<nav class='navbar navbar-expand-lg navbar-dark justify-content-center'><a class='navbar-brand text-danger' href='galleryj.html'>Cosmic Visions</a><button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button><div class='collapse navbar-collapse'id='navbarNav'>  <ul class='navbar-nav'><li class='nav-item active'> <a class='nav-link' href='index.html'>Home <span class='sr-only'>(current)</span></a></li><li class='nav-item'><a class='nav-link' href='galleryJ.html'>Gallery</a></li><li class='nav-item'> <a class='nav-link' href='joelbio.html'>Artist Statement/Bio</a></li><li class='nav-item'><a class='nav-link' href='contact.html'>Contact/Purchase</a></li></ul></div></nav></div>";
