4d0813fee89bd7dee40b8578e611f17659c961c6
Joel Rudin <jarud55@gmail.com>
2024-10-10 12:51:06 -0400

    Gallery layout, gen layout

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.querySelector(
   ".navLoad"
 ).innerHTML = `	<nav class='navbar navbar-expand-lg navbar-dark'>
 		<a class="navbar-brand text-danger" href='index.html'>Cosmic Visions</a>
+		
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -39,10 +40,14 @@ document.querySelector(
 				</li>
 					
 			</ul>
-	          <audio controls src='Track06a.mp3' id='audio' loop='loop' autoplay='autoplay'></audio> 
+	      
 			<!-- <form class="form-inline my-2 my-lg-0">
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 			</form> -->
 		</div> 
 	</nav>`;
+
+document.getElementById(
+  "audioLoad"
+).innerHTML = `<audio controls src='Track06a.mp3' id='audio' loop='loop' autoplay='autoplay'></audio>`;
