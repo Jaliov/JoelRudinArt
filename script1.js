363b4dd049ec51cdf7ee2dcf676f9eacf683c1ac
Joel Rudin <jarud55@gmail.com>
2024-12-28 15:37:16 -0500

    reinstate missing style, js files

---
+++ script1.js
@@ -0,0 +1,26 @@
+document.querySelector("#imSlderLoad").innerHTML = `<div class="banner">
+        <div class="slider" style="--quantity: 10">
+            <div class="item" style="--position: 1"><img src="images/Scarlet_Night.png" alt=""> </div>
+            <div class="item" style="--position: 2"><img src="images/wishingsmll2.png" alt=""> </div>
+            <div class="item" style="--position: 3"><img src="images/TheBurgeoning.png", " alt=""> </div>
+            <div class="item" style="--position: 5"><img src="images/Night_Reverie.png" alt=""> </div>
+            <div class="item" style="--position: 6"><img src="images/Boundary.png" alt=""> </div>
+            <div class="item" style="--position: 7"><img src="images/Guardian.png" alt=""> </div>
+            <div class="item" style="--position: 8"><img src="images/NordicNight-2.png" alt=""> </div>
+            <div class="item" style="--position: 9"><img src="images/PlanetProximus_edit.png" alt=""> </div>
+            <div class="item" style="--position: 10"><img src="images/Industry_web.png" alt=""> </div>
+		    <div class="item" style="--position: 4"><img src="images/Planet5.png" alt=""> </div>
+        </div>
+          <div class="content">
+               <header>
+            	<h1 id ="title">
+				Cosmic Visions</h1>
+                </header>
+            <div class="author">
+             
+               <div id="homeNotice">
+				<p class="text-light"> Art by Joel Rudin on a site built by Joel Rudin</p>
+            </div>
+       
+        </div>
+    </div>`;
